6aab3689a693b3778e22d299dbdf09f5fb3a6d16
Sai Akshith V <akshith292@gmail.com>
2021-07-11 21:14:15 +0530

    console logging details

--- routes/indexPage.js
+++ routes/indexPage.js
@@ -7,70 +7,69 @@ const request = require('request');
 var postsLists = [];
 
 router.get('/', (req, resp) => {
-
 	resp.render('indexPage');
 });
-router.post('/search',  function (req, res){
-
+router.post('/search',async  function (req, res){
 	const searchTag=req.body.searchField;
-	const URL = `https://medium.com/tag/${searchTag}/latest`;
+	const URL = `https://medium.com/search?q=${searchTag}`;
+	const URL1 = `https://medium.com/tag/${searchTag}/latest`;
+	request(URL1, function (err, res, body) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			let $ = cheerio.load(body);
+			console.log("**********************************");
+			console.log("Searched Tag Name :- ")
+			//getting tag name and related tags
+			const $tag = $(".ap.aq.l");
+			const $tagName = $tag.find("h2")
+			console.log($tagName.text());
+			console.log("**********************************");
+			console.log("Related Tag Names :- ")
+			$(".t.bk.do a").each(function (index, relatedTag) {
+				const $rTagName = $(relatedTag);
+				const $relatedTagName = $rTagName.find(".di");
+				console.log($relatedTagName.text());
+			})
+		}
+	});
 	request(URL, function (err, res, body) {
-    if(err)
-    {
-        console.log(err);
-    }
-    else
-    {
-        let $ = cheerio.load(body);  //loading of complete HTML body
-		// $(".u-paddingTop20").each(function (index, postData) {
-		// 	const $element = $(postData);
-		// 	const $title = $element.find("h3");
-		// 	const $responsesNumber = $element.find(".buttonSet a");
-		// 	const $postLink = $element.find(".postArticle-readMore a");
-		// 	const $authorName = $element.find(
-		// 	".postMetaInline-authorLockup a.u-accentColor--textDarken"
-		// 	);
-		// 	const $authorImage = $element.find('.u-relative img');
-		// 	const $publishDate = $element.find("time");
-		// 	const list = {
-		// 		title: $title.text(),
-		// 		responsesNumber: $responsesNumber.text(),
-		// 		postId: $postLink.attr("data-post-id"),
-		// 		authorImage: $authorImage.attr('src'),
-		// 		authorName: $authorName.text(),
-		// 		date: $publishDate.text(),
-		// 	};
-		// 	console.log(list);
-	 	// });
+		if (err) {
+			console.log(err);
+		}
+		else {
+			let $ = cheerio.load(body);
+			console.log("**********************************");
+			console.log(`Articles List on ${searchTag} :-`)
+			$(".u-paddingTop20").each(function (i, element) {
+				const $element = $(element);
+				const $title = $element.find("h3");
+				const $responses = $element.find(".buttonSet a");
+				const $link = $element.find(".postArticle-readMore a");
+				const $author = $element.find(".postMetaInline-authorLockup a.u-accentColor--textDarken");
+				const $publishDate = $element.find("time");
+				const $readingTime = $element.find(".readingTime");
+				const list = {
+					title: $title.text(),
+					responses: $responses.text(),
+					link: $link.attr("data-post-id"),
+					author: $author.text(),
+					date: $publishDate.text(),
+					readingTime: $readingTime.attr("title")
+				};
+				postsLists.push(list);
+			});
+		}
+		console.log(postsLists);
+		console.log("**********************************");
 		
-		
-		
-		// console.log($('.ae.gk'));
-		// $(".ae.gk").each(function (index, postData) {
-		// 	const $element = $(postData);
-		// 	const $authorName = $element.find('.ay.hj.dj.hk.ez.hl.fb.fc.fd.fe.ff.bb.hm');
-		// 		const list = {
-		// 			authorName: $authorName.text()
-		// 		}
-		// 		console.log(list);
-			
-		// });
-
-		//getting tag name and related tags
-		const $tag = $(".ap.aq.l");
-		const $tagName = $tag.find("h2")
-		console.log($tagName.text());
-
-		$(".t.bk.do a").each(function (index, relatedTag) {
-			const $rTagName = $(relatedTag);
-			const $relatedTagName = $rTagName.find(".di");
-			console.log($relatedTagName.text());
-		})
-
-    }
-});
+	});
+	
 	res.render('indexPage');
-
+	
 });
+
+
 
 module.exports = router;
